d345569e795c2df3ec06b316c2ab22b0b0726504
boxhock <contact@jonashals.me>
2020-01-07 21:33:04 +0100

    Remove unnecessary semicolons

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const rp = require('request-promise');
-const snx = require('synthetix');
-const Decimal = require('decimal.js');
+const rp = require('request-promise')
+const snx = require('synthetix')
+const Decimal = require('decimal.js')
 
-const host = 'bravenewcoin-v1.p.rapidapi.com';
-const url = 'https://' + host + '/convert';
+const host = 'bravenewcoin-v1.p.rapidapi.com'
+const url = 'https://' + host + '/convert'
 
 const getPriceData = async (synth) => {
   return rp({
@@ -19,25 +19,25 @@ const getPriceData = async (synth) => {
     },
     json: true
   })
-};
+}
 
 const calculateIndex = (indexes) => {
-  let value = new Decimal(0);
+  let value = new Decimal(0)
   indexes.forEach(i => {
     value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData.to_quantity)))
-  });
+  })
   return value.toNumber()
-};
+}
 
 const createRequest = async (input, callback) => {
-  const asset = input.data.asset || 'sCEX';
-  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted);
-  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase());
+  const asset = input.data.asset || 'sCEX'
+  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted)
+  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase())
   await Promise.all(data.index.map(async (synth) => {
     synth.priceData = await getPriceData(synth)
-  }));
+  }))
 
-  data.result = calculateIndex(data.index);
+  data.result = calculateIndex(data.index)
 
   callback(200, {
     jobRunID: input.id,
@@ -45,19 +45,19 @@ const createRequest = async (input, callback) => {
     result: data.result,
     statusCode: 200
   })
-};
+}
 
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
     res.status(statusCode).send(data)
   })
-};
+}
 
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data)
   })
-};
+}
 
 exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
@@ -67,6 +67,6 @@ exports.handlerv2 = (event, context, callback) => {
       isBase64Encoded: false
     })
   })
-};
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
