6cf40ae46e4cd5d39f53dfde127493b6803d2e9d
boxhock <contact@jonashals.me>
2020-01-07 21:26:10 +0100

    Change URL to RapidAPI

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-const rp = require('request-promise')
-const snx = require('synthetix')
-const Decimal = require('decimal.js')
+const rp = require('request-promise');
+const snx = require('synthetix');
+const Decimal = require('decimal.js');
+
+const host = 'bravenewcoin-v1.p.rapidapi.com';
+const url = 'https://' + host + '/convert';
 
 const getPriceData = async (synth) => {
   return rp({
-    url: 'https://api.bravenewcoin.com/chainlink/gwa-historic',
+    url: url,
     headers: {
-      'X-Chainlink-API-Key': process.env.API_KEY
+      'x-rapidapi-host': host,
+      'x-rapidapi-key': process.env.API_KEY
     },
     qs: {
-      market: 'USD',
-      coin: synth.symbol
+      qty: 1,
+      to: 'USD',
+      from: synth.symbol
     },
     json: true
   })
-}
+};
 
 const calculateIndex = (indexes) => {
-  let value = new Decimal(0)
+  let value = new Decimal(0);
   indexes.forEach(i => {
-    value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData.data[0][1])))
-  })
+    value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData.to_quantity)))
+  });
   return value.toNumber()
-}
+};
 
 const createRequest = async (input, callback) => {
-  const asset = input.data.asset || 'sCEX'
-  const datas = snx.getSynths({ network: 'mainnet' }).filter(({ index, inverted }) => index && !inverted)
-  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase())
+  const asset = input.data.asset || 'sCEX';
+  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted);
+  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase());
   await Promise.all(data.index.map(async (synth) => {
     synth.priceData = await getPriceData(synth)
-  }))
+  }));
 
-  data.result = calculateIndex(data.index)
+  data.result = calculateIndex(data.index);
 
   callback(200, {
     jobRunID: input.id,
@@ -40,19 +45,19 @@ const createRequest = async (input, callback) => {
     result: data.result,
     statusCode: 200
   })
-}
+};
 
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
     res.status(statusCode).send(data)
   })
-}
+};
 
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data)
   })
-}
+};
 
 exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
@@ -62,6 +67,6 @@ exports.handlerv2 = (event, context, callback) => {
       isBase64Encoded: false
     })
   })
-}
+};
 
-module.exports.createRequest = createRequest
+module.exports.createRequest = createRequest;
